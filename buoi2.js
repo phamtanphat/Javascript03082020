2a014f2548c0c29b49386101ca6c3ddc818d7aec
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-08-07 20:32:58 +0700

    for in for of

---
+++ buoi2.js
@@ -0,0 +1,26 @@
+// 9 : Vòng lặp
+// for of
+// Array
+// const arrayNumbers = [5,4,2,0,1,2,3,4,5,10]
+// for(var value of arrayNumbers){
+//     console.log(value)
+// }
+// Object
+// không sử dụng object khi duyệt for of
+
+// for in
+// Array
+// const arrayNumbers = [5,4,2,0,1,2,3,4,5,10]
+// for (const key in arrayNumbers) {
+//     console.log(key)
+// }
+// Object
+// const teo = {
+//     name : "Nguyen Van Teo",
+//     age : 10,
+//     1: 20
+// }
+// for (const key in teo) {
+//     console.log(typeof key)
+// }
+
