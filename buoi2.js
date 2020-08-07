fd613114e1da3fecb401590aac1f1f72781e2e5b
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-08-07 21:08:19 +0700

    bai tap 2 : for in

--- buoi2.js
+++ buoi2.js
@@ -25,7 +25,7 @@
 // }
 
 // Ví dụ
-var a = [1, 2, 4, 8, 16];
+// var a = [1, 2, 4, 8, 16];
 /**
  * Sử dụng vòng lặp for để hiển thị ra màn hình các phần tử của mảng a theo thứ tự ngược lại:
  * 16
@@ -34,7 +34,55 @@ var a = [1, 2, 4, 8, 16];
  * 2
  * 1
  */
-for (var i = a.length - 1 ; i >= 0 ; i--){
-    console.log(a[i])
+// for (var i = a.length - 1 ; i >= 0 ; i--){
+//     console.log(a[i])
+// }
+
+/**
+ * Sử dụng vòng lặp for...in để in ra tất cả các key trong object sau, in ra cả các key của các nested object (object con lồng bên trong object lớn)
+ * Gợi ý: đây là bài tập khó, bạn nên tìm hiểu về recursive trước
+ */
+
+var apartment = {
+    bedroom: {
+      area: 20,
+      bed: {
+        type: 'twin-bed',
+        price: 100
+      }
+    }
+  };
+  
+function getkey(object){
+    for (const key in object) {
+        console.log(key)
+        for(const keyBedRomm in object[key]){
+            console.log(keyBedRomm)
+            for(const keyBed in object[key][keyBedRomm]){
+            console.log(keyBed)
+            }
+        }
+    }
 }
+  
+function getkey(object){
+    for (const key in object) {
+        console.log(key)
+        if(typeof object[key] === "object" ){
+            getkey(object[key])
+        }
+    }
+}
+  getkey(apartment)
+  
+  /**
+   * Kết quả mong muốn:
+   * bedroom
+   * area
+   * bed
+   * type
+   * price
+   * Chú ý: không cần hiển thị ra đúng thứ tự như trên
+   */
+  
 
