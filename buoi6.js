19e7cac8517fee396d2393df290890120e9d4c02
phatdroid94@gmail.com <phatdroid94@gmail.com>
2020-08-17 20:25:17 +0700

    default parameters

--- buoi6.js
+++ buoi6.js
@@ -17,14 +17,42 @@
 // }
   
 // a.run()
-var a = {
-    foo : 'bar' ,
-    run : function() {
-      setTimeout(() =>{
-        console.log(this.foo)
-      },2000)
-    }
-  }
-a.run()
+// var a = {
+//     foo : 'bar' ,
+//     run : function() {
+//       setTimeout(() =>{
+//         console.log(this.foo)
+//       },2000)
+//     }
+//   }
+// a.run()
+
+//21 : Template string
+
+// function greeting(name){
+//     return 'Hi, ' + name + '!'
+//   }
   
+//   function greeting(name){
+//     return `Hi, ${name} !`
+//   }
+//   console.log(greeting("Phat")
+
+// 22 : argument
+
+// Array like object
+
+// function tinhTong(){
+//     const newArr = Array.from(arguments)
+//     return newArr.reduce((accum , current) => accum + current,0)
+
+// }
+// console.log(tinhTong(1,2,3,4,5,6,7,8,9,10))
+
+// 23 : Default parameters
+// function tinhTong(a = 0, b = 0){
+//     console.log(a + b)
+// }
   
+// tinhTong(10)
+
